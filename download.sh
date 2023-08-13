19f33c1f56526aa4133551e44c29b19aced70c9d
scruffy <yishuiwang163@gmail.com>
2023-08-13 21:36:37 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir tsm
+cd tsm
+wget https://raw.githubusercontent.com/yishuiwang/tsm.sh/main/tsm.sh
+wget https://raw.githubusercontent.com/yishuiwang/tsm.sh/main/config.txt
+wget https://raw.githubusercontent.com/yishuiwang/tsm.sh/main/README.md
